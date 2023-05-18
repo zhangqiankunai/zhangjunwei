7d363cbff4ba812fa36aef97cd33c6bdff235347
zhangqk3@lenovo.com <zhangqk3@lenovo.com>
2023-05-18 13:39:58 +0800

    feat: 增加路由

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+import "antd/dist/reset.css";
 function App() {
-  return <div className="App">这是主页</div>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">首页</Link>
+          </li>
+          <li>
+            <Link to="/about">关于我们</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/about/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>你好世界</div>
+  )
+}
